cd76fe6d5476b45a72a45824bb5e4b23ba3f26bd
Niranjan <33854825+niranjanreddy891@users.noreply.github.com>
2018-05-30 10:22:19 +0530

    Create ROI.py

---
+++ pclpy/tests/ROI.py
@@ -0,0 +1,38 @@
+import pclpy
+from pclpy import *
+from pclpy import pcl
+import os
+import pclpy.view.vtk
+import numpy as np
+
+
+def test_open_in_new_window():
+    pc = pclpy.io.read("test.las", "PointXYZRGBA")
+    viewer = pcl.visualization.PCLVisualizer("viewer", True)
+    viewer.addPointCloud(pc)
+    viewer.resetCamera()
+
+    viewers = [viewer]
+
+    def handle_event_area(event):
+        # use x to toggle rectangle selection
+        assert isinstance(event, pcl.visualization.AreaPickingEvent)
+        indices = pcl.vectors.Int()
+        event.getPointsIndices(indices)
+
+        other_viewer = pcl.visualization.PCLVisualizer("viewer", True)
+        rgb = np.array([pc.r[indices], pc.g[indices], pc.b[indices]]).T
+        other_pc = pcl.PointCloud.PointXYZRGBA.from_array(pc.xyz[indices], rgb)
+        other_viewer.addPointCloud(other_pc)
+        other_viewer.resetCamera()
+        viewers.append(other_viewer)
+
+    viewer.registerAreaPickingCallback(handle_event_area)
+
+    while not all(v.wasStopped() for v in viewers):
+        for v in viewers:
+            if not v.wasStopped():
+                v.spinOnce(50)
+
+
+test_open_in_new_window()
